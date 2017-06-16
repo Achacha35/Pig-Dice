1a85c23df0331f1d4f50ad5cd49fcf762f338b40
Achacha35 <achachapeter@gmail.com>
2017-06-16 15:33:50 +0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//business logic
+
+function Dice (total, tally) {
+  this.total = totalScore;
+  this.tally = tallyScore;
+}
+
+Dice.prototype.randomNumber = function() {Math.floor(Math.random() * 6) + 1);
+return randomNumber
+this.tally = randomNumber + this.tally;
+}
+
+
+//U
