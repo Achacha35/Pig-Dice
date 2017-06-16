a794f8d2f6f80e8d38eb8ccf261614194a832c54
Achacha35 <achachapeter@gmail.com>
2017-06-16 16:50:11 +0300

    third

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,23 @@ function Dice (total, tally) {
   this.tally = tallyScore;
 }
 
-Dice.prototype.randomNumber = function() {Math.floor(Math.random() * 6) + 1);
+Dice.prototype.randomNumber = function() {Math.floor(Math.random() * 6) + 1;
 return randomNumber
 this.tally = randomNumber + this.tally;
-}
+};
+
+var playerOne = Dice(0, 0);
+var playerTwo = Dice(0, 0);
+
 
 
 //User interface
 $("document").ready(function() {
-  $("form#pig-dice").submit(event() {
-    event.preventDefault
+  $("form#pig-dice").submit(function(event) {
+    event.preventDefault();
 
-    var firstPlayer = $("input#player-1").val();
-    var secondPlayer = $("input#player-2").val();
 
-     var newDice = new Dice (firstPlayer, secondPlayer);
-     
 
-  }
-}
+  $("ul#secondplayer").append("randomNumber")
+   });
+     });
